b27ae25003e33c05abb5544b98e80be271461cce
dutamfr30 <dutamfr@gmail.com>
2023-10-28 15:51:39 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+        <p className='text-base'>Hello World!</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'purple': '#581c87'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
